e8663b3514afc6b950ad03c89fb049a76e214b09
Nontanun Ausungnoen <nunx111@gmail.com>
2025-01-27 07:49:01 +0700

    Add layout of Signup page

--- screens/Signup.tsx
+++ screens/Signup.tsx
@@ -1,40 +1,59 @@
 import { Component } from 'react'
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Dimensions} from 'react-native';
+import { updateEmail, updatePassword } from '@/actions/users'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+const screenWidth = Dimensions.get("window").width
+const screenHeight = Dimensions.get("window").height
 
 export default class Signup extends React.Component {
-
-  state = {
-    niceNumber: 0
-  }
-
-  adding = () => {
-    this.setState({niceNumber: this.state.niceNumber+1})
-  }
-  substract = () => {
-    this.setState({niceNumber: this.state.niceNumber-1})
-  }
-
-  setToZero = () =>{
-    this.setState({niceNumber:0})
-  }
 
   render(){
     return (
       <View style={{   flex: 1,  backgroundColor: 'blue',  alignItems: 'center',  justifyContent: 'center',}}>
-          <Text>
-            Signup page
-          </Text>
-
-            <TouchableOpacity 
-            style={{backgroundColor:'black', margin:10, padding:10, borderRadius:5}}
-            onPress={()=> this.props.navigation.navigate('Login')}>
-                <Text style={{color:'white'}}>Go to login page</Text>
-            </TouchableOpacity>
+              <View style={{width:screenWidth*0.9,  marginTop:10, }}>
+                <Text style={{left:15}}>Username</Text>
+              </View>
+              <View style={{width:screenWidth*0.9,  marginTop:10, }}>
+                <Text style={{left:15}}>Email</Text>
+              </View>
+              <TextInput 
+              style={{height: 50, width:screenWidth*0.9,  color:'black', paddingHorizontal:20, margin:0, borderRadius:10, borderColor:'grey', borderWidth:1}}
+              placeholderTextColor={'grey'}
+              placeholder={'example@example.com'}
+              // value={this.props.user.email}
+              // onChangeText={input=>this.props.updateEmail(input)}
+              onChangeText={input=>this.props.updateEmail(input)}
+              value={this.props.user.email}
+              />
+              <View style={{width:screenWidth*0.9,  marginTop:10, }}>
+                <Text style={{left:15}}>Password</Text>
+              </View>
+              <TextInput 
+              style={{height: 50, width:screenWidth*0.9,  color:'black', paddingHorizontal:20, margin:0, borderRadius:10, borderColor:'grey', borderWidth:1}}
+              placeholderTextColor={'grey'}
+              placeholder={'Passcode123'}
+              onChangeText={input=>this.props.updatePassword(input)}
+              value={this.props.user.password}
+              secureTextEntry={true}
+              />
       </View>
     );
   }
 }
 
 
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({ updateEmail, updatePassword }, dispatch)
+}
+const mapStateToProps = (state) => {
+  return{
+    user: state.user,
+  }
+}
+
+
+export default connect (mapStateToProps, mapDispatchToProps)(Signup)

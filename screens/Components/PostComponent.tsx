46394c81144b26d87f950dd1ffdc03ad99cb5d85
Nontanun Ausungnoen <nunx111@gmail.com>
2025-01-27 10:22:32 +0700

    Add function to present post in home page

---
+++ screens/Components/PostComponent.tsx
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+import { View, Text, ScrollView, Image, Dimensions, TouchableOpacity } from 'react-native';
+import PropTypes from 'prop-types'
+const screenWidth = Dimensions.get('window').width;
+
+export default class PostComponent extends Component {
+
+    static propTypes = {
+        prop: PropTypes
+    };
+
+    state = {
+        numLike: 0,
+        liked: undefined
+    }
+    render() {
+        const { item } = this.props;
+        const { liked } = this.state;
+
+        return (
+            <View>
+                <View style={{ width: screenWidth, height: 60, backgroundColor: 'white', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                    <View style={{ justifyContent: 'center', alignItems: 'center', flexDirection: 'row' }}>
+
+                        <View style={{ width: 40, height: 40, margin: 10, borderRadius: 20, backgroundColor: 'rgba(0,0,0,0.1)', justifyContent: 'center', alignItems: 'center' }}>
+                            <Text>
+                                {item.username ? item.username.charAt(0).toUpperCase() : '?'}
+                            </Text>
+                        </View>
+                        <Text style={{ fontWeight: 'bold', fontSize: 18 }}>{item.username}</Text>
+                    </View>
+                    {
+                        item.date ? new Date(item.date.seconds * 1000).toLocaleDateString() : 'Date not available'
+                    }
+                </View>
+                <View style={{ width: screenWidth, height: 360 }}>
+                    <ScrollView
+                        horizontal={true}
+                        pagingEnabled={true}
+                        showsHorizontalScrollIndicator={false}
+                    >
+                        {item.photos?.map((e, index) => (
+                            <Image key={index} source={{ uri: e }} style={{ width: screenWidth, height: 360 }} />
+                        ))}
+                    </ScrollView>
+                    <View style={{width:screenWidth, flexDirection:'row',justifyContent:'space-between'}}>
+                        <View style={{justifyContent:'center',flexDirection:'row',alignItems:'center'}}>
+                            <Text>Likes</Text>
+                            <text>Comment</text>
+                        </View>
+                    </View>
+                </View>
+            </View>
+        );
+    }
+}
--- screens/TabScreens/HomeScreen.tsx
+++ screens/TabScreens/HomeScreen.tsx
@@ -1,27 +1,49 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, TouchableOpacity, TextInput, Dimensions, Image} from 'react-native';
+import { FlatList, Text, View, Button, TouchableOpacity, TextInput, Dimensions, Image} from 'react-native';
 
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { getUser } from '@/actions/users';
+import { getPosts } from '@/actions/post';
+
+import PostComponent from '@/screens/Components/PostComponent';
+
+const screenHeight = Dimensions.get('window').height
+const screenWidth = Dimensions.get('window').width
+
 
 class HomeScreen extends React.Component {
 
-    render(){
-        return (
-            <View style={{flex:1, backgroundColor:'white', justifyContent:'center', alignItems:'center'}}>
-                <Text style={{fontSize:35, fontFamily:'logo-font', marginVertical:60, color:'#0095f6'}}>HomeScreen</Text>
-            </View>
-        );
-    }
+    componentDidMount = () => {
+        this.props.getPosts()
+      }
+      render(){
+  
+          return (
+              <View style={{flex:1, backgroundColor:'white', justifyContent:'center', alignItems:'center'}}>
+                  <FlatList
+                  data={this.props.post.feed}
+                  keyExtractor={(item) => JSON.stringify(item.uid)}
+                  renderItem={({item}) => (
+                      <PostComponent
+                      item={item}
+                      user={this.props.user}
+                      />
+                  )}
+                  />
+              </View>
+          );
+      }
+  
 }
 
 const mapDispatchToProps = (dispatch) => {
-    return bindActionCreators({ getUser}, dispatch)
+    return bindActionCreators({ getUser, getPosts }, dispatch)
 }
 const mapStateToProps = (state) => {
     return{
         user: state.user,
+        post: state.post
     }
 }
 
